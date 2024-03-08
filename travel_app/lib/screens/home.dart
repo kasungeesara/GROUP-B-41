88474db86513e0f58d12126181ab0999ceb7cc88
TharushaPerera27070 <134818242+TharushaPerera27070@users.noreply.github.com>
2024-03-08 11:39:46 +0530

    add

--- travel_app/lib/screens/home.dart
+++ travel_app/lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:travel_app/screens/profile.dart';
 import 'package:travel_app/widgets/bar_carousal.dart';
 import 'package:travel_app/widgets/cafe_carousal.dart';
@@ -49,19 +50,16 @@ class _HomeScreenState extends State<HomeScreen> {
           setState(() {
             _selectedIndex = 1;
           });
-
         }
         if (index == 2) {
           setState(() {
             _selectedIndex = 2;
           });
-
         }
         if (index == 3) {
           setState(() {
             _selectedIndex = 3;
           });
-
         }
       },
       child: Container(
@@ -122,17 +120,139 @@ class _HomeScreenState extends State<HomeScreen> {
             if (_selectedIndex == 1) const HotelCarousel(),
             if (_selectedIndex == 2) const FineDineCarousel(),
             if (_selectedIndex == 3) const BestCafeCarousel(),
-            const SizedBox(
-              height: 10.0,
-            ),
-           
+
+            //const SizedBox(height: 10.0,),
+
             if (_selectedIndex == 1) const HotelCarousel(),
             if (_selectedIndex == 2) const PubCarousel(),
             if (_selectedIndex == 3) const CoffeeCarousel(),
-            const SizedBox(
-              height: 10.0,
-            ),
+
+            //const SizedBox(height: 10.0,),
+
             if (_selectedIndex == 2) const BarCarousel(),
+            if (_selectedIndex == 0)
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Container(
+                  height: 200,
+                  width: double.infinity,
+                  child: Stack(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          image: const DecorationImage(
+                            image: AssetImage("assets/k20.jpg"),
+                            fit: BoxFit.cover,
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 10,
+                        right: 90,
+                        left: 90,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text(
+                            "Keep exploring",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: "Outfit-Regular",
+                              color: Colors.black,
+                              letterSpacing: 0.5,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const Positioned(
+                        top: 60,
+                        left: 90,
+                        child: Text(
+                          "Discover more in \n          Kandy",
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: "Outfit-Regular",
+                            color: Color.fromARGB(255, 249, 249, 249),
+                            letterSpacing: 0.5,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            const SizedBox(
+              height: 25,
+            ),
+            if (_selectedIndex == 0) const DestinationCarousel(),
+            if (_selectedIndex == 0)
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Container(
+                  height: 600,
+                  width: double.infinity,
+                  child: Stack(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          image: const DecorationImage(
+                            image: AssetImage("assets/surf5.jpg"),
+                            fit: BoxFit.cover,
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 10,
+                        left: 12,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text(
+                            "Read more",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: "Outfit-Regular",
+                              color: Colors.black,
+                              letterSpacing: 0.5,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const Positioned(
+                        top: 350,
+                        left: 10,
+                        child: Text(
+                          "10 best places to \n surf in\n Sri lanka",
+                          style: TextStyle(
+                            fontSize: 35,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: "Outfit-Regular",
+                            color: Color.fromARGB(255, 249, 249, 249),
+                            letterSpacing: 0.5,
+                          ),
+                        ),
+                      ),
+                      const Positioned(
+                        bottom: 60,
+                        left: 10,
+                        child: Text(
+                          "The south and east coasts of Sri Lanka \nare home to the best surf spots",
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: "Outfit-Regular",
+                            color: Color.fromARGB(255, 249, 249, 249),
+                            letterSpacing: 0.5,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
           ],
         ),
       ),
