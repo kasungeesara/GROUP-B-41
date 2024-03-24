7ac799a6c31f589eabba12d920cc4fa6b3b0ce2c
Navindu Dissanayake <navindu158@gmail.com>
2024-03-24 23:29:20 +0530

    change

--- travel_app/lib/screens/map.dart
+++ travel_app/lib/screens/map.dart
@@ -1,8 +1,5 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:location/location.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
